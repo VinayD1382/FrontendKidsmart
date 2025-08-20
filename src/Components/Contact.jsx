6846343d88f41daadaa581ea13090b7bf09ef600
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2025-08-20 13:36:13 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -17,7 +17,7 @@ function Contact() {
   const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    await axios.post("http://backendkidsmart.onrender.com/api/contact", formData);
+    await axios.post("https://backendkidsmart.onrender.com/api/contact", formData);
 
     setSubmitted(true);
     setFormData({ name: "", email: "", message: "" }); // Reset form
